9e3237356c79f5fdd78e67a5e4ddf5e9954ac8a0
FrancescoAngelozzi <francesco.angelozzi2002@gmail.com>
2026-06-09 00:30:37 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
-export default defineConfig({
-  base: "/frangelozzi/",
-  plugins: [react()],
-});
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <App />
+);
